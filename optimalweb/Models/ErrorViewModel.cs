18dd01797bab43062586374fe3888cca2fe09b43
linhnq <linhnq@jits.com.vn>
2023-11-20 16:23:35 +0700

    Init project

---
+++ optimalweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace optimalweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
